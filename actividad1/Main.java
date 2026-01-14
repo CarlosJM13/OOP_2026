8556d03856413751476d52994b4ac7d0379862b4
Carlos J. Mendez <car1253hyt@gmail.com>
2026-01-14 10:13:26 -0600

    Delete actividad1 directory

--- actividad1/Main.java
+++
@@ -1,8 +0,0 @@
-package edu_carlos_mendez.actividad1;
-import edu_carlos_mendez.actividad1.ui.CLI;
-
-public class Main {
-    public static void main(String[] args) {
-        CLI.start();
-    }
-}
