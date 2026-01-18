e278f5b0a271b1a7944e8569c9cf5d33a12effe0
Carlos J. Méndez <car1253hyt@gmail.com>
2026-01-18 13:11:14 -0600

    Actividad 1 se basa en un sistema de venta de vehículos en consola.
    La clase Vehiculo contiene la estructura de datos para marca, modelo y precio.
    
    Catalogo contiene la lógica para almacenar la lista y calcular el total.
    
    Ticket contiene el código para mostrar el menú, capturar datos e imprimir el recibo.
    
    Main ejecuta el código iniciando la interfaz.

--- src/edu_carlos_mendez/actividad1/Main.java
+++ src/edu_carlos_mendez/actividad1/Main.java
@@ -1,8 +1,9 @@
 package edu_carlos_mendez.actividad1;
-import edu_carlos_mendez.actividad1.ui.CLI;
+import edu_carlos_mendez.actividad1.ui.Ticket;
 
 public class Main {
     public static void main(String[] args) {
-        CLI.start();
+       Ticket interfaz = new Ticket();
+        interfaz.mostrarMenu();
     }
 }
