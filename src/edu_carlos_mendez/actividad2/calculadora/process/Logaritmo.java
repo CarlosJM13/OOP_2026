f13584f0bcaa6497e2de73681fef0c01f6e1bb06
Carlos J. Mendez <car1253hyt@gmail.com>
2026-01-20 10:51:24 -0600

    Refactor de Logaritmo,java para usar divisiones sucesivas.
    Se ha actualizado el método de cálculo del logaritmo para utilizar divisiones sucesivas en lugar de multiplicaciones.

--- src/edu_carlos_mendez/actividad2/calculadora/process/Logaritmo.java
+++ src/edu_carlos_mendez/actividad2/calculadora/process/Logaritmo.java
@@ -2,23 +2,25 @@ package edu_carlos_mendez.actividad2.calculadora.process;
 
 public class Logaritmo {
     /**
-     * Este método calcula el logaritmo entero de un número en una base logarítmica
+     * Este método calcula el logaritmo entero de un número en una base logarítmica con divisiones sucesivas
      * @param op1 base del log
      * @param op2 numero del log
      * @return el exponente
      */
+
     public static int realizarOperacion(int op1, int op2) {
-        int exponente = 0;
-        int valorActual = 1;
+        if (op1 <= 1) return 0;
 
-        while (valorActual < op2) {
-            valorActual = multiplicacion.realizarOperacion(valorActual, op1);
-            exponente++;
+        int contador = 0;
+        int actual = op2;
+
+        while (actual >= op1) {
+            actual = Division.realizarOperacion(actual, op1);
+            contador = Suma.realizarOperacion(contador, 1);
         }
 
-        if (valorActual > op2) {
-            return exponente - 1;
-        }
-        return exponente;
+        return contador;
     }
 }
+
+//editado para poder hacerlo con divisiones sucesivas. Error: se ejecutaba con multiplicaciones
